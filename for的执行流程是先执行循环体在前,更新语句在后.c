29f802d8c85ff0e31135c9109b33eb55f358922b
zhulasong <jh20000316@163.com>
2024-05-25 22:34:17 +0800

    Add files via upload

---
+++ for的执行流程是先执行循环体在前,更新语句在后.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//#include <time.h>
+
+int main(){
+	int a = 0, b = 0;
+	for (a = 1, b = 1; a <= 100; a++){
+		if (b >= 20)
+			break;
+		if (b % 3 == 1){
+			b = b + 3;
+			continue;
+		}
+		b = b - 5;
+	}
+	printf("%d\n", a);
+	system("pause");
+	return 0;
+}
